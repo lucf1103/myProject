b6d4c1067db8ab1ca229ab4d52861e91aad77f89
卢超凡 <lucf1103@163.com>
2018-08-22 14:25:44 +0800

    发送短信，编辑内容（多人）

--- app/src/main/java/freestyle/lu/android/myapplication333/MainActivity.java
+++ app/src/main/java/freestyle/lu/android/myapplication333/MainActivity.java
@@ -4,6 +4,7 @@ package freestyle.lu.android.myapplication333;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -37,8 +38,46 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                setSMS();
             }
         });
     }
-}
+
+    private void setSMS(){
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+        smsIntent.setData(Uri.parse("smsto:"));
+        smsIntent.setType("vnd.android-dir/mms-sms");//必须指定type
+        smsIntent.putExtra("address", "15158200175,18768490574");//address字段不能改 (用，号分割  可以添加多个手机号)
+        smsIntent.putExtra("sms_body", "测试");//sms_body 不能改
+        startActivity(smsIntent);
+    }
+
+    protected void sendMessageByInterface2() {
+
+//
+//        String phoneNumber = vSendNo.getText().toString().trim();
+//        String message = vSendContent.getText().toString().trim();
+//
+//        SmsManager sms = SmsManager.getDefault();
+//
+//        Intent sentIntent = new Intent(SENT_SMS_ACTION);
+//        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, sentIntent, 0);
+//
+//        Intent deliverIntent = new Intent(DELIVERED_SMS_ACTION);
+//        PendingIntent deliverPI = PendingIntent.getBroadcast(this, 0, deliverIntent, 0);
+//
+//        if (message.length() > 70) {
+//            ArrayList<String> msgs = sms.divideMessage(message);
+//            ArrayList<PendingIntent> sentIntents =  new ArrayList<PendingIntent>();
+//
+//            for(int i = 0;i<msgs.size();i++){
+//                sentIntents.add(sentPI);
+//            }
+//            sms.sendMultipartTextMessage(phoneNumber, null, msgs, sentIntents, null);
+//        } else {
+//            sms.sendTextMessage(phoneNumber, null, message, sentPI, deliverPI);
+//        }
+
+    }
+
+ }
